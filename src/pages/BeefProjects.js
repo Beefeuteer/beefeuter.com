8563bc4f55e05400ad25ef85935ff36d4c62efb0
Bugra <bugra@sahratelekom.com>
2023-02-22 11:47:52 +0300

    add link to the text "github profile" on https://beefeuter.com/projects

--- src/pages/BeefProjects.js
+++ src/pages/BeefProjects.js
@@ -3,13 +3,21 @@ import Container from "react-bootstrap/Container";
 function BeefProjects() {
   return (
     <Container className="d-flex justify-content-center align-items-center text-center">
-      <h5>
+      <h6>
         So I am still designing this site and this page is not finished sorry
         .-.
         <br /> <br />
-        But you can check my github profile for my projects. I will update this
-        page soon.
-      </h5>
+        But you can check my
+        <a
+          href="https://github.com/Beefeuteer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {"  "}
+          github profile{"  "}
+        </a>
+        for my projects. I will update this page soon.
+      </h6>
     </Container>
   );
 }
